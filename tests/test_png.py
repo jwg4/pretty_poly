59ba227983dd41cffe6e7564cbe89d8e0ad2abe3
Jack Grahl <jack.grahl@gmail.com>
2021-02-21 01:31:17 +0000

    Add a test for png construction.

---
+++ tests/test_png.py
@@ -0,0 +1,10 @@
+from pretty_poly import make_png
+
+
+def test_basic_png_right_size():
+    tiling = [
+        [(0, 1), (0, 0)],
+        [(1, 1), (1, 0)]
+    ]
+    image = make_png(tiling)
+    assert image.size = 13, 13
