0e7efeb3a3be186f5633df59fde46f75ed946eb5
Jack Grahl <jack.grahl@gmail.com>
2021-03-10 00:01:18 +0000

    Add a test for the black lines picture.

--- tests/test_image.py
+++ tests/test_image.py
@@ -1,4 +1,4 @@
-from pretty_poly.image import make_colored_blocks
+from pretty_poly.image import make_colored_blocks, make_black_lines
 
 from examples.right_12 import TILING as RIGHT_12
 
@@ -20,3 +20,14 @@ def test_large_nonrectangular_tiling():
     assert len(color_data[0]) == 130
     assert len(palette) == 43, str(palette)
     assert (0, 0, 0) in palette
+
+
+def test_make_black_lines():
+    tiling = [
+        [(0, 0), (0, 1), (0, 2), (1, 1)],
+        [(1, 0), (2, 0), (2, 1), (3, 0)]
+    ]
+    color_data, palette = make_colored_blocks(tiling)
+    assert len(color_data) == 49
+    assert len(color_data[0]) == 59
+    assert len(palette) == 2, str(palette)
