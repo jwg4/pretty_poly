278f7b24636200b961a371fa9925853bf20ff0af
Jack Grahl <jack.grahl@gmail.com>
2021-03-11 00:00:43 +0000

    Make another example.

--- examples/make.py
+++ examples/make.py
@@ -1,5 +1,8 @@
 from pretty_poly.png import write_colored_blocks_png, write_lines_png
 
+from .right_12 import TILING as RIGHT_12
+
 
 def write_examples():
     write_lines_png("examples/lines.png", [[(0, 0), (0, 1), (1, 0)], [(1, 1)]])
+    write_colored_blocks_png("examples/right_12.png", RIGHT_12)
