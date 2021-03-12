f375d8e8715f9cc6f03a842c9453aab4893cd67f
Jack Grahl <jack.grahl@gmail.com>
2021-03-12 00:31:15 +0000

    Try to do svg.

---
+++ pretty_poly/svg.py
@@ -0,0 +1,28 @@
+import svgwrite
+
+from .image import make_colored_blocks, make_lines, make_blocks_and_lines
+from .abstract import make_design
+
+
+def svg_color(rgb):
+    """
+    >>> from pretty_poly.svg import svg_color
+    >>> svg_color((123, 234, 45))
+    'rgb(123, 234, 45)'
+
+    """
+    return "rgb(%d, %d, %d)" % rgb
+
+
+def write_colored_blocks_svg(filename, tiling, scale=10):
+    faces, _, _, _ = make_design(tiling)
+    data, palette = make_colored_blocks(tiling, scale)
+    size = len(data[0]), len(data)
+    dwg = svgwrite.Drawing(filename, size=data)
+    for row, y in enumerate(faces):
+        for sq, x in enumerate(row):
+            color = svg_color(palette[sq])
+            square = dwg.rect((x * scale, y * scale), (scale, scale), fill=color)
+            dwg.add(square)
+
+    dwg.save()
