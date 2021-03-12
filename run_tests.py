9b79f0b1083654462ce13b38c98cb116a85ac8dd
Jack Grahl <jack.grahl@gmail.com>
2021-03-12 00:31:15 +0000

    Try to do svg.

--- run_tests.py
+++ run_tests.py
@@ -15,4 +15,10 @@ def run_doctest():
         filepath = os.path.join("examples/", file) 
         doctest.testfile(filepath, optionflags=doctest.ELLIPSIS)
 
+    for file in os.listdir("pretty_poly/"):
+        if not file.endswith(".py"):
+            continue
+        filepath = os.path.join("pretty_poly/", file) 
+        doctest.testfile(filepath)
+
     doctest.testfile("README.md")
