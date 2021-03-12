59d1e1c730b055362f35c1c847a14f84ad78575a
Jack Grahl <jack.grahl@gmail.com>
2021-03-12 00:34:24 +0000

    Make svg in the examples.

--- examples/make.py
+++ examples/make.py
@@ -1,6 +1,7 @@
 import os
 
 from pretty_poly.png import write_colored_blocks_png, write_lines_png
+from pretty_poly.svg import write_colored_blocks_svg
 
 from .right_12 import TILING as RIGHT_12
 
@@ -13,3 +14,4 @@ def write_examples():
     write_lines_png("examples/images/lines.png", [[(0, 0), (0, 1), (1, 0)], [(1, 1)]])
     write_lines_png("examples/images/thick.png", RIGHT_12, 10, 3)
     write_colored_blocks_png("examples/images/right_12.png", RIGHT_12)
+    write_colored_blocks_svg("examples/images/right_12.svg", RIGHT_12)
