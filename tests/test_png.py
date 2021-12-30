c1fe33ada0e20a4615869aca8f12941abac2ce0f
Jack Grahl <jack.grahl@gmail.com>
2021-12-30 16:08:14 +0000

    Add a test.

--- tests/test_png.py
+++ tests/test_png.py
@@ -24,3 +24,16 @@ def test_large_nonrectangular_tiling():
     width, height, rows, info = r.read()
     assert rows is not None
     assert info is not None
+
+
+def test_thick_lines_and_margins():
+    f, filename = tempfile.mkstemp(suffix=".png")
+    size, thickness = 10, 3
+    expected_width = 4 * size + 3 * thickness
+    expected_height = 4 * size + 3 * thickness
+    tiles = [[(0, 0), (0, 1), (1, 0)], [(1, 1)]]
+    write_lines_png(filename, tiles, size, thickness)
+    r = png.Reader(filename)
+    width, height, rows, info = r.read()
+    assert width == expected_width
+    assert height == expected_height
